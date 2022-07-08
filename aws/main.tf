5b092730eafb5ac6cf0f66a6fcb8096451843191
KC <noreply@kcemenike.github.com>
2022-07-08 08:49:39 -0300

    add variables

--- aws/main.tf
+++ aws/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
 
