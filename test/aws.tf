78d3ec9ddda83b6ba7b39568eeb82b5233489d11
KC <noreply@kcemenike.github.com>
2022-07-08 16:02:43 -0300

    add bucket with TF

--- test/aws.tf
+++ test/aws.tf
@@ -19,14 +19,14 @@ provider "aws" {
 
 }
 
-variable "ID" {
+variable "ID" { # we overrode this in the command line of terraform run
   type = string
 }
 
 resource "aws_cloudformation_stack" "bucket" {
   name = "Test-Stack"
   parameters = {
-    ID = var.ID
+    bucketID = var.ID # we parsed this value
   }
   template_body = file("${path.module}/frontend.yml")
 }
