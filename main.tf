ba8a8eafd77c73f6e885fd7aec4e166f9fa5c1de
KC <noreply@kcemenike.github.com>
2022-07-08 06:54:23 -0300

    deployed nginx with terraform

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  # added for compatibilty with Docker Desktop for Windows
+  host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
